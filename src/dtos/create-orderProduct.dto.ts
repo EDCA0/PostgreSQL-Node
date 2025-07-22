9d40b5e44fcba1bee08beee04c738e3f61e81e51
EDCA <eric.cavanzo@outlook.com>
2025-07-22 09:35:22 -0500

    EMPIEZO DE NUEVO CURSO DE SEGURIDAD

--- src/dtos/create-orderProduct.dto.ts
+++ src/dtos/create-orderProduct.dto.ts
@@ -1,39 +1,39 @@
 import { IsInt, IsNotEmpty, IsPositive, Min } from 'class-validator';
 
 export class CreateOrderProductDto {
-    @IsNotEmpty({
-        message: 'El customerId no puede estar vacio',
-    })
-    @IsInt({
-        message: 'El customerId debe de ser un entero',
-    })
-    @IsPositive({
-        message: 'El customerId debe ser un numero positivo',
-    })
-    declare customer: number;
+	@IsNotEmpty({
+		message: 'El customerId no puede estar vacio',
+	})
+	@IsInt({
+		message: 'El customerId debe de ser un entero',
+	})
+	@IsPositive({
+		message: 'El customerId debe ser un numero positivo',
+	})
+	declare customer: number;
 
-    @IsNotEmpty({
-        message: 'El productId no puede estar vacio',
-    })
-    @IsInt({
-        message: 'El productId debe de ser un entero',
-    })
-    @IsPositive({
-        message: 'El productId debe ser un numero positivo',
-    })
-    declare product: number;
+	@IsNotEmpty({
+		message: 'El productId no puede estar vacio',
+	})
+	@IsInt({
+		message: 'El productId debe de ser un entero',
+	})
+	@IsPositive({
+		message: 'El productId debe ser un numero positivo',
+	})
+	declare product: number;
 
-    @IsNotEmpty({
-        message: 'El productId no puede estar vacio',
-    })
-    @IsInt({
-        message: 'El productId debe de ser un entero',
-    })
-    @IsPositive({
-        message: 'El productId debe ser un numero positivo',
-    })
-    @Min(1, {
-        message: 'La cantidad no puede ser menor a $constraint1'
-    })
-    declare amount: number;
+	@IsNotEmpty({
+		message: 'El productId no puede estar vacio',
+	})
+	@IsInt({
+		message: 'El productId debe de ser un entero',
+	})
+	@IsPositive({
+		message: 'El productId debe ser un numero positivo',
+	})
+	@Min(1, {
+		message: 'La cantidad no puede ser menor a $constraint1',
+	})
+	declare amount: number;
 }

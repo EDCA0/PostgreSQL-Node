0e147564282670494dde74fcdadbe28fd37cd9b6
EDCA <eric.cavanzo@outlook.com>
2025-07-31 12:32:59 -0500

    feat(auth): Añadir generación y verificación de JWT con la librería `jose`
    Se introduce la funcionalidad básica para la creación y validación de JSON Web Tokens (JWT) como paso inicial para implementar la autenticación en la API.
    
    - Se añade la dependencia `jose` al proyecto para el manejo de JWT.
    - Se crea el script `token-sign.ts` que contiene una función para firmar un payload y generar un token.
    - Se crea el script `token-verify.ts` con una función para validar la firma y la integridad de un token existente.
    - Se define la interfaz `MyTokenPayload` para estandarizar la estructura del cuerpo del token.

---
+++ token-sign.ts
@@ -0,0 +1,38 @@
+import { JWTPayload, SignJWT } from 'jose';
+
+export interface MyTokenPayload extends JWTPayload {
+  sub: string;
+  role: string;
+  permissions?: string[]; // La propiedad 'permissions' es opcional
+  // Añade aquí cualquier otro dato que esperes en el payload
+}
+
+
+export const secret = 'myCat' //❗ Debe de estar en una variable de ambiente, ❗ Como deberia llamarse la variable de ambiente?
+const payload = {
+    sub: "1", //❗ Que significa sub
+    role: 'customer' //❗ Aca va el scope, Que cuenta como scope, que se deberia poner?
+}
+
+async function signToken (payload: MyTokenPayload, secret: string) {
+ // jose requiere que el secreto sea una Uint8Array.
+  // Es crucial que el secreto sea robusto y mantenido de forma segura.
+  const secretBytes = new TextEncoder().encode(secret);
+
+   const token = await new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' }) // Define el algoritmo de firma. HS256 es común.
+    .setIssuedAt() // Establece el tiempo de emisión del token. Buena práctica.
+    // .setExpirationTime('1h') // Opcional: añade un tiempo de expiración (ejemplo: '1h', '2 days', etc.)
+    // .setIssuer('urn:example:issuer') // Opcional: Quién emitió el token
+    // .setAudience('urn:example:audience') // Opcional: Para quién es el token
+    .sign(secretBytes); // Firma el token con el secreto
+
+    return token;
+}
+
+async function prueba() {
+    const token = await signToken(payload, secret);
+    console.log(token);
+}
+
+prueba()
